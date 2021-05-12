efe46cc0cbe735ce9a04b2a0b0850b5310528283
chenyifei03 <chenyifei03@baidu.com>
2021-05-13 00:24:12 +0800

    deploy & backup

--- install.sh
+++ install.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-  # todo: backup dbfile
+
   WORK_DIR=`pwd`
   APP_NAME="tale"
   CODE_DIR="tale-vyfe"
   APP_PROD=${APP_NAME}-prod
 
+  # todo: backup dbfile
+  time2=$(date "+%Y%m%d%H")
+  backfile=tale.db."${time2}"
+  cp $APP_PROD/resources/tale.db /home/work/backup/"${backfile}"
+
   if [ ! -d ${APP_PROD} ];
   then
       mkdir ${APP_PROD}
   fi
-  # todo: maven package
+  # maven package
   cd $CODE_DIR
   rm -rf target/*
   mvn clean package -Pprod -Dmaven.test.skip=true
   # todo: copy tar from output dir
+  cd $WORK_DIR
   if test -f $APP_NAME.tar.gz
   then
 	  rm -rf $APP_NAME.tar.gz
@@ -21,8 +27,20 @@
 
   cp $CODE_DIR/target/dist/$APP_NAME.tar.gz $WORK_DIR
   # 解压部署
+  cd $APP_PROD && sh tool stop
+  echo 'stopped'
+  cd $WORK_DIR
+  rm -rf $APP_PROD/*
+
   tar -zxvf $APP_NAME.tar.gz -C $APP_PROD && cd $APP_PROD
   chmod +x tool
   echo 'unzip OK'
+  # replace schema.sql
+  cd $WORK_DIR/backup && sqlite3 ${backfile} .dump > ${backfile}.sql
+  cd $WORK_DIR
+  mv backup/${backfile}.sql $APP_PROD/resources/schema.sql
+  # install.lock(stop repeat install)
+  sleep 2
+  touch $APP_PROD/resources/install.lock
   sleep 1
-  sh tool restart
+  cd $APP_PROD && sh tool restart
